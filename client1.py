b4879d1a88beba57baf4bf2e406824649f13ee2f
Margarita Otochkina <margaret.otochkina@gmail.com>
2015-09-23 14:18:41 -0400

    Update client1.py

--- client1.py
+++ client1.py
@@ -4,19 +4,20 @@ import socket, sys, pickle
 server = socket.socket()
 server.connect(("localhost", 1234))
 stat = ''
-path = './'
 
-#send command to a server
+#send comd
 server.send("ls")
+#recive status
 stat = server.recv(1024)
 print stat
-server.send(path)
-#get list of files
+#send dir
+server.send('./')
+#recieve files in dir
 data = server.recv(4096)
-#deserialize recieved list
+#deserialize aray
 d1 = pickle.loads(data)
 
-#print file names
+#print
 for i in range(0, len(d1)):
     print d1[i]
 server.close()
