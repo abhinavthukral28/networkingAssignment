43929493ffa8f669c89cffea0e8020c4da51aa86
Margarita Otochkina <margaret.otochkina@gmail.com>
2015-09-23 13:46:44 -0400

    Update client1.py

--- client1.py
+++ client1.py
@@ -1,12 +1,17 @@
 import socket, sys, pickle
 
+#connect to server
 server = socket.socket()
 server.connect(("localhost", 1234))
 
+#send command to a server
 server.send("ls")
+#get list of files
 data = server.recv(4096)
+#deserialize recieved list
 d1 = pickle.loads(data)
 
+#print file names
 for i in range(0, len(d1)):
     print d1[i]
 server.close()
