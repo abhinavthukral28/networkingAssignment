a497c504d4bce652f6087d4f0005a6d9c838f7e2
Margarita Otochkina <margaret.otochkina@gmail.com>
2015-09-23 13:48:24 -0400

    Update server1.py

--- server1.py
+++ server1.py
@@ -1,18 +1,21 @@
 import socket, sys, pickle, os
 
+#create socket
 server = socket.socket()
 server.bind(("localhost", 1234))
 server.listen(1)
 
+#get list of filenames in curr directory
 path = "./"
 dirs = os.listdir( path )
 
 
 while True:
+    #accept client
     remoteSocket, remoteAddr = server.accept()
-    cmd = remoteSocket.recv(1024)
+    cmd = remoteSocket.recv(1024) #recieve command
     if(cmd == 'ls'):
-        data = pickle.dumps(dirs)
-        remoteSocket.send(data);
+        data = pickle.dumps(dirs) #serialize an array
+        remoteSocket.send(data); #send an array
 server.close()
 
